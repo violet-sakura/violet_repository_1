a6d526fea59e2458135154f7388e0734a58b8ed1
violet <2414811214@qq.com>
2025-03-31 16:12:50 +0800

    添加 main.cpp 文件

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+void violet-sort(int* arr, const int len);
+
+int main(void)
+{
+	int arr1[] = { 11, 14, 5, 22, 15, 6 };
+	int len1 = sizeof(arr1) / sizeof(arr1[0]);
+	
+	violet-sort(arr1, len1);
+
+
+	return 0;
+}
